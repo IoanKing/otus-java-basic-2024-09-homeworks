e516dc2996937b1f10063325fab09ab992e4db21
IvanSuslov <ioankingisepp@mail.ru>
2024-12-16 00:30:33 +0300

    Fix mentor tips and comments. Refactoring.

--- src/main/java/ru/otus/java/basic/homework20/MainApp.java
+++ src/main/java/ru/otus/java/basic/homework20/MainApp.java
@@ -2,51 +2,79 @@ package ru.otus.java.basic.homework20;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainApp {
+    final static String DEFAULT_PATH = ".";
+    final static String DEFAULT_FILE_TYPE = ".txt";
+
     public static void main(String[] args) {
         System.out.println("=========================================");
         System.out.println("===== Домашнее задание. Лекция №20 ======");
         System.out.println("=========================================");
-
-        final String DEFAULT_PATH = ".";
-        final String DEFAULT_FILE_TYPE = ".txt";
-        File file = new File(DEFAULT_PATH);
         String[] fileList;
+        String changedFile;
+        fileList = getFileList();
+        changedFile = actionFileSelect(fileList);
+        printFile(changedFile);
+        inputInFile(changedFile);
+        printFile(changedFile);
+    }
 
+    /**
+     * Выбор файла из директории.
+     *
+     * @param fileList список доступных файлов.
+     * @return название файла
+     */
+    private static String actionFileSelect(String[] fileList) {
         Scanner scanner = new Scanner(System.in);
-        int command = 0;
+        int command;
 
-        if (!file.exists()) {
-            System.out.println("Директория не существует.");
-            return;
-        }
-
-        System.out.println("Список файлов в корневой директории c расширением \"" + DEFAULT_FILE_TYPE + "\": ");
-        fileList = file.list(new FileTypeFilter(DEFAULT_FILE_TYPE));
-        if (fileList.length == 0) {
-            System.out.println("В директории нет файлов с расширением \"" + DEFAULT_FILE_TYPE + "\"");
-            return;
-        }
         for (int i = 0; i < fileList.length; i++) {
-            System.out.println( (i+1) + ") " + fileList[i]);
+            System.out.println((i + 1) + ") " + fileList[i]);
         }
 
         do {
             System.out.println("Выберите файл для работы: ");
             command = scanner.nextInt();
             if (command <= fileList.length && command > 0) {
-                printFile(fileList[command-1]);
-                inputInFile(fileList[command-1]);
-                printFile(fileList[command-1]);
-                break;
+                return fileList[command - 1];
             }
         } while (true);
     }
 
+    /**
+     * Метод получает список файлов из корневой директории проекта с расширением ".txt".
+     *
+     * @return список названий файлов
+     */
+    private static String[] getFileList() {
+        File file = new File(DEFAULT_PATH);
+        if (!file.exists()) {
+            System.out.println("Директория не существует.");
+            return null;
+        }
+        String[] fileList;
+        System.out.println("Список файлов в корневой директории c расширением \"" + DEFAULT_FILE_TYPE + "\": ");
+        fileList = file.list(new FileTypeFilter(DEFAULT_FILE_TYPE));
+        assert fileList != null;
+        if (fileList.length == 0) {
+            System.out.println("В директории нет файлов с расширением \"" + DEFAULT_FILE_TYPE + "\"");
+            return null;
+        }
+        return fileList;
+    }
+
+    /**
+     * Метод производить запись введенной с клавиатуры строчки в файл из корневой директории проекта.
+     *
+     * @param fileName название файла.
+     */
     private static void inputInFile(String fileName) {
+        if (fileName == null) {
+            return;
+        }
         Scanner scanner = new Scanner(System.in);
         String newLine;
         System.out.println("Введите строчку текста для записи в файл");
@@ -62,15 +90,20 @@ public class MainApp {
         }
     }
 
+    /**
+     * Метод выводить в консоль содержимое текстового файла из корневой директории проекта.
+     *
+     * @param fileName название файла.
+     */
     private static void printFile(String fileName) {
-        try (   FileInputStream fis = new FileInputStream(fileName);
-                BufferedInputStream bis = new BufferedInputStream(fis);
-                InputStreamReader in = new InputStreamReader(bis) ) {
+        try (FileInputStream fis = new FileInputStream(fileName);
+             BufferedInputStream bis = new BufferedInputStream(fis);
+             InputStreamReader in = new InputStreamReader(bis)) {
             int n = in.read();
             System.out.println("Содержимое файла \"" + fileName + "\"");
             System.out.println("-------------------");
             while (n != -1) {
-                System.out.print((char)n);
+                System.out.print((char) n);
                 n = in.read();
             }
             System.out.println("\n-------------------");
