aa427cfac8e378945afb978a2381e5ece73ac09b
IvanSuslov <ioankingisepp@mail.ru>
2025-01-25 23:53:12 +0300

    Release homework for lesson 25

---
+++ client/src/main/java/ru/otus/java/basic/homework25/Client.java
@@ -0,0 +1,89 @@
+package ru.otus.java.basic.homework25;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Client {
+    private Socket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
+    private Scanner scanner;
+
+    public Client() throws IOException {
+        scanner = new Scanner(System.in);
+        socket = new Socket("localhost", 8189);
+        out = new DataOutputStream(socket.getOutputStream());
+        in = new DataInputStream(socket.getInputStream());
+
+        new Thread(() -> {
+            try {
+                while (true) {
+                    String message = in.readUTF();
+                    if (message.startsWith("/")) {
+                        if (message.equalsIgnoreCase("/exitok")) {
+                            break;
+                        }
+                        if (message.equalsIgnoreCase("/kickoff")) {
+                            break;
+                        }
+                        if (message.startsWith("/authok ")) {
+                            System.out.println("Удалось успешно войти в чат с именем пользователя "
+                                    + message.split(" ")[1]);
+                        }
+                        if (message.startsWith("/regok ")) {
+                            System.out.println("Удалось успешно зарегистрироваться с именем пользователя "
+                                    + message.split(" ")[1]);
+                        }
+                    } else {
+                        System.out.println(message);
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("Ошибка при установлении соединения: " + e.getMessage());
+                e.printStackTrace();
+            } finally {
+                disconnect();
+            }
+        }).start();
+
+        while (true) {
+            String message = scanner.nextLine();
+            out.writeUTF(message);
+            if (message.equalsIgnoreCase("/exit")) {
+                break;
+            }
+        }
+    }
+
+    public void disconnect() {
+        try {
+            if (in != null) {
+                in.close();
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при отключении соединения: " + e.getMessage());
+            e.printStackTrace();
+        }
+        try {
+            if (out != null) {
+                out.close();
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при отключении соединения: " + e.getMessage());
+            e.printStackTrace();
+        }
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при отключении соединения: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return;
+    }
+}
---
+++ server/src/main/java/ru/otus/java/basic/homework25/Server.java
@@ -0,0 +1,86 @@
+package ru.otus.java.basic.homework25;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Server {
+    private int port;
+    private List<ClientHandler> clients;
+    private AuthenticatedProvider authenticatedProvider;
+
+    public Server(int port) {
+        this.port = port;
+        clients = new CopyOnWriteArrayList<>();
+        authenticatedProvider = new InMemoryAuthenticatedProvider(this);
+    }
+
+    public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер запущен на порту: " + port);
+            authenticatedProvider.initialize();
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new ClientHandler(socket, this);
+            }
+
+        } catch (IOException e) {
+            System.err.println("Ошибка при запуске сервера: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+        broadcastMessage("В чат зашёл: " + clientHandler.getUsername());
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+        broadcastMessage("Из чата вышел: " + clientHandler.getUsername());
+    }
+
+    public void broadcastMessage(String message) {
+        for (ClientHandler c : clients) {
+            c.sendMsg(message);
+        }
+    }
+
+    public void clientMessage(String message, String clientName) {
+        for (ClientHandler c : clients) {
+            if (c.getUsername().equals(clientName)) {
+                c.sendMsg(message);
+            }
+        }
+    }
+
+    public void kickUser(String clientName) {
+        for (ClientHandler c : clients) {
+            if (c.getUsername().equals(clientName)) {
+                c.sendMsg("Вы были отключены от чата администратором.");
+                c.sendMsg("/exitok");
+//                c.disconnect();
+                break;
+            }
+        }
+    }
+
+    public boolean isUsernameBusy(String username) {
+        for (ClientHandler c : clients) {
+            if (c.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<ClientHandler> getClients() {
+        return clients;
+    }
+
+    public AuthenticatedProvider getAuthenticatedProvider() {
+        return authenticatedProvider;
+    }
+}
